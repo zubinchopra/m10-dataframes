5b1651dc6f9f692f0ba310d19d8b0ab32ab97ea3
bdinh <baodinh96@gmail.com>
2017-04-07 01:15:33 -0700

    Added 2016 data, fixed some question, added bonus

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,18 +1,18 @@
-# Exercise 7: Husky Football 2015 Season
-# Read in the Husky Football 2015 game data into a variable called `husky.games.2015`
-husky.games.2015 <- read.csv('data/huskies_2015.csv')
+# Exercise 7: Husky Football 2016 Season
+# Read in the Husky Football 2016 game data into a variable called `husky.games.2016`
+husky.games.2016 <- read.csv('data/huskies_2016.csv')
 
 # Create a vector of the teams that the Huskies played against during that season
 # Call this vector `not.huskies`. You'll need to convert this column to a vector
-not.huskies <- as.vector(husky.games.2015$opponent)
+not.huskies <- as.vector(husky.games.2016$opponent)
 
 # Create a vector of the their final scores for the games
 # Call this variable `husky.scores`
-husky.scores <- husky.games.2015$uw.score
+husky.scores <- husky.games.2016$uw_score
 
 # Create 2 variables called `rushing.yards` and `passing.yards` to represent the yards the Huskies rushed and passed
-rushing.yards <- husky.games.2015$rushing.yards
-passing.yards <- husky.games.2015$passing.yards
+rushing.yards <- husky.games.2016$rushing_yards
+passing.yards <- husky.games.2016$passing_yards
 
 # Create a variabled called `combined.yards` that is the total yardage of the Huskies for each game
 combined.yards <- passing.yards + rushing.yards
@@ -41,7 +41,20 @@ MostYardsScore <- function(games) {
   return(paste("The game that the Huskies had the most yards was against", opponent, "on", date, "where they scored", highest.score, "points!"))
 }
 
-# What was the highest yardage game so far this season?
-# Hint: Read in the dataset titled `huskies_2016.csv` and save it as a variable
-husky.games.2016 <- read.csv('data/huskies_2016.csv')
-most.yards.2016 <- MostYardsScore(husky.games.2016)
+# What was the highest yardage game so far last season?
+# Hint: Read in the dataset titled `huskies_2015.csv` and save it as a variable
+husky.games.2015 <- read.csv('data/huskies_2015.csv')
+most.yards.2015 <- MostYardsScore(husky.games.2015)
+
+### Bonus ###
+# When working with data, you will often find yourself manually adding data into your dataset.
+# This bonus will help you practice such skills.
+# Using resources online, find out whether each game was played at home or away.
+# Create a vector `where.played` that corresponds to what you found.
+# Add the vector `where.played` as a new column to `husky.games.2016`
+# Hint: For bowl games simply listing "bowl" will be fine.
+
+where.played <- c("home", "home", "home", "away", "home", "away", "home", "away", "away",
+                  "home", "home", "away", "bowl", "bowl")
+
+husky.games.2016$where.played <- where.played

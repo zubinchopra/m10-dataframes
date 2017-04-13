7b164a34f36f426c8ca6cb373cdf93e8b8b374d6
zubinchopra <zubin.msvv@gmail.com>
2017-04-12 17:52:31 -0700

    done

--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,38 +1,39 @@
 # Exercise 3: Working with Data Frames
 
 # Load R's "USPersonalExpenditure" dataest using the `data()` function
-
+data("USPersonalExpenditure")
 
 # The variable USPersonalExpenditure is now accessible to you. Unfortunately, it's not a data.frame
 # Test this using the is.data.frame function
-
+is.data.frame(USPersonalExpenditure)
 
 # Luckily, you can simply pass the USPersonalExpenditure variable to the data.frame function
 # to convert it a data.farme
 
-# Create a new variable by passing the USPersonalExpenditure to the data.frame function
 
+# Create a new variable by passing the USPersonalExpenditure to the data.frame function
+us.pexp <- data.frame(USPersonalExpenditure)
 
 # What are the column names of your dataframe?
-
+colnames(us.pexp)
 
 # Why are they so strange?
 
 
 # What are the row names of your dataframe?
-
+rownames(us.pexp)
 
 # Create a column `category` that is equal to your rownames
-
+us.pexp$category <- rownames(us.pexp)
 
 # How much money was spent on personal care in 1940?
-
+care.1940 <- us.pexp["Personal Care", "X1940"]
 
 # How much money was spent on Food and Tobacco in 1960
-
+food.1960 <- us.pexp["Food and Tobacco", "X1960"]
 
 # What was the highest expenditure category in 1960?
-
+exp.1960 <- us.pexp$category[us.pexp$X1960 == max(us.pexp$X1960)]
 
 ### Bonus ###
 
--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,34 +2,36 @@
 
 # Read data into a variable called `grants` using the `read.csv` function
 # Make sure *not* to read the strings as factors
+grants <- read.csv("data/gates_money.csv", stringsAsFactors = FALSE)
 
 # Use the View function to look at your data
-
+View(grants)
 
 # Create a variable `spending` as the `total_amount` column of the dataset
-
+spending <- grants$total_amount
 
 # Confirm that your `spending` variable is a vector using the `is.vector` function
-
+is.vector(spending)
 
 # Create a variable `org` as the `organization` column of the dataset.
-
+org <- grants$organization
 
 ### Now you can ask some more interesting questions about the dataset.  Store your answers in variables ###
 
 # What was the mean grant value?
-
+mean.grant.value <- mean(spending)
 
 # What was the dollar amount of the largest grant?
-
+max.amount <- max(spending)
 
 # What was the dollar amount of the smallest grant?
-
+least.amount <- min(spending)
 
 # Which organization received the largest grant?
-
+max.recipient <- org[spending == max.amount]
 
 # Which organization received the smallest grant?
-
+min.recipient <- org[spending == least.amount]
 
 # How many grants were awarded in 2010?
+length(spending[grants$start_year] == 2010)
